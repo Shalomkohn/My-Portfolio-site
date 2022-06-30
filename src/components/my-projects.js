8a084971bc715e7664dd38720afa6a5df6f5055e
Shalomkohn <sbk5775@gmail.com>
2022-06-30 16:18:31 +0300

    added sams calking, changed project shaddow on all screens, changed img on game, put footer back to bottom

--- src/components/my-projects.js
+++ src/components/my-projects.js
@@ -1,9 +1,18 @@
 import wordBeaterImg from '../word-beater-img.png'
 import NCFurnitureImg from '../NC-furniture-img.png'
+import samsCaulkingImg from '../sams-caulking-img.png'
 
 const projectsArray = [
     {
         id: 1,
+        title: 'Sams Shower Caulking',
+        date: 'jul/2022',
+        img: samsCaulkingImg,
+        aTagHref: 'https://dev8171.dtiqg5g6eferp.amplifyapp.com',
+        srcCodeHref: 'https://github.com/Shalomkohn/sams-caulking',
+    },
+    {
+        id: 2,
         title: 'NC Furniture',
         date: 'Jun/2022',
         img: NCFurnitureImg,
@@ -11,7 +20,7 @@ const projectsArray = [
         srcCodeHref: 'https://github.com/Shalomkohn/North-capital-furniture',
     },
     {
-        id: 2,
+        id: 3,
         title: 'WordBeater Game ',
         date: 'Feb/2022',
         img: wordBeaterImg,
@@ -39,7 +48,7 @@ const MyProjects = () => {
                     </div>        
                 )
             })}
-            <div className='project'>
+            <div className='project' style={{height: '350px'}}>
                 <h3 className='projectName'>Another cool project being worked on right now...</h3>
             </div>  
         </div>
