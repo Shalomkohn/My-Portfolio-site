12ae9fc75f699928a40c0e79782e088e3c426538
Shalomkohn <sbk5775@gmail.com>
2022-06-30 13:30:43 +0300

    gave my projects a shaddow instead of a border

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -9,7 +9,7 @@ const AboutMe = () => {
             i really enjoy front end development and web design.
             <br />
             <br />
-            I have a passion for problem solving and just getting things to work in general.
+            I have a passion for problem solving and getting things to work in general.
             <br />
             <br />
             This website is fully original, made with just React.js and css, feel free to check out the <a className="thisWebsiteSrcHrefATag" href="https://github.com/Shalomkohn/My-Portfolio-site"><i>source code</i></a> on GitHub. 
