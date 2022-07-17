c40158f9aa822be8f63dada205ce0bf9a3cc54a0
Shalomkohn <sbk5775@gmail.com>
2022-07-18 01:34:29 +0300

    added my skills added max-width to aboutMeDiv, took off some margin

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -13,6 +13,9 @@ const AboutMe = () => {
                 I have a passion for problem solving and getting things to work in general.
                 <br />
                 <br />
+                I am skilled in:<br /><br /> React.js <i className="fa-brands fa-react"></i><br /> Html <i className="fa-brands fa-html5"></i><br /> Css <i className="fa-brands fa-css3-alt"></i><br /> Javascript <i className="fa-brands fa-js"></i><br /> Bootstrap <i className="fa-brands fa-bootstrap"></i><br />Sass <i className="fa-brands fa-sass"></i>
+                <br />
+                <br />
                 This website is fully original, made with just React.js and css, feel free to check out the <a className="thisWebsiteSrcHrefATag" href="https://github.com/Shalomkohn/My-Portfolio-site"><i>source code</i></a> on GitHub. 
                 <br />
                 <br />
