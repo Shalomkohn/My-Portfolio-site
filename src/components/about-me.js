3ad14aab0043e74cabf2db74289155ae6e16854b
Shalomkohn <sbk5775@gmail.com>
2022-07-18 16:30:14 +0300

    changed dimentions on aboutMeDiv put logos in front of skills

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -13,8 +13,13 @@ const AboutMe = () => {
                 I have a passion for problem solving and getting things to work in general.
                 <br />
                 <br />
-                I am skilled in:<br /><br /> React.js <i className="fa-brands fa-react"></i><br /> Html <i className="fa-brands fa-html5"></i><br /> Css <i className="fa-brands fa-css3-alt"></i><br /> Javascript <i className="fa-brands fa-js"></i><br /> Bootstrap <i className="fa-brands fa-bootstrap"></i><br />Sass <i className="fa-brands fa-sass"></i>
-                <br />
+                I am skilled in:<br /><br />
+                <i className="fa-brands fa-react"> </i> React.js <br />
+                <i className="fa-brands fa-html5"></i> Html <br />
+                <i className="fa-brands fa-css3-alt"> </i> Css <br />
+                <i className="fa-brands fa-js"></i> Javascript <br />
+                <i className="fa-brands fa-bootstrap"> </i> Bootstrap <br />
+                <i className="fa-brands fa-sass"></i> Sass<br />
                 <br />
                 This website is fully original, made with just React.js and css, feel free to check out the <a className="thisWebsiteSrcHrefATag" href="https://github.com/Shalomkohn/My-Portfolio-site"><i>source code</i></a> on GitHub. 
                 <br />
